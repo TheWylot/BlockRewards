0baaa0255882d0316c9bdd7194233c0a6e175844
TheWylot <thewylot22@gmail.com>
2024-12-15 19:16:59 +0330

    build: removed update checker and increased to 1.2

--- src/main/java/me/niko302/blockrewards/BlockRewards.java
+++ src/main/java/me/niko302/blockrewards/BlockRewards.java
@@ -1,8 +1,5 @@
 package me.niko302.blockrewards;
 
-import com.jeff_media.updatechecker.UpdateCheckSource;
-import com.jeff_media.updatechecker.UpdateChecker;
-import com.jeff_media.updatechecker.UserAgentBuilder;
 import lombok.Getter;
 import me.niko302.blockrewards.commands.BlockRewardsCommand;
 import me.niko302.blockrewards.config.ConfigManager;
@@ -45,35 +42,6 @@ public class BlockRewards extends JavaPlugin implements Listener {
 
         // Initialize metrics
         new Metrics(this, 22496);
-
-        // Initialize update checker
-        new UpdateChecker(this, UpdateCheckSource.SPIGOT, "117755")
-                .setNotifyRequesters(false)
-                .setNotifyOpsOnJoin(false)
-                .setUserAgent(UserAgentBuilder.getDefaultUserAgent())
-                .checkEveryXHours(12)
-                .onSuccess((commandSenders, latestVersion) -> {
-                    String messagePrefix = configManager.getPrefix();
-                    String currentVersion = getDescription().getVersion();
-
-                    if (currentVersion.equalsIgnoreCase(latestVersion)) {
-                        String updateMessage = color(messagePrefix + "&aYou are using the latest version of BlockRewards!");
-                        Bukkit.getConsoleSender().sendMessage(updateMessage);
-                        Bukkit.getOnlinePlayers().stream().filter(ServerOperator::isOp).forEach(player -> player.sendMessage(updateMessage));
-                        return;
-                    }
-
-                    List<String> updateMessages = List.of(
-                            color(messagePrefix + "&cYour version of BlockRewards is outdated!"),
-                            color(String.format(messagePrefix + "&cYou are using %s, latest is %s!", currentVersion, latestVersion)),
-                            color(messagePrefix + "&cDownload latest here:"),
-                            color("&6")
-                    );
-
-                    Bukkit.getConsoleSender().sendMessage(updateMessages.toArray(new String[]{}));
-                    Bukkit.getOnlinePlayers().stream().filter(ServerOperator::isOp).forEach(player -> player.sendMessage(updateMessages.toArray(new String[]{})));
-                })
-                .onFail((commandSenders, e) -> {}).checkNow();
 
         // Load player message preferences
         loadData();
